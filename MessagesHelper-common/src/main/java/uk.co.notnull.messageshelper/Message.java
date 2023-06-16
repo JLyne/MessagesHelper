c81576de1c73a25157f147628113da73d5014a98
James Lyne <jim+github@not-null.co.uk>
2023-06-16 16:38:11 +0100

    Initial codebase

---
+++ MessagesHelper-common/src/main/java/uk.co.notnull.messageshelper/Message.java
@@ -0,0 +1,96 @@
+package uk.co.notnull.messageshelper;
+
+import net.kyori.adventure.text.ComponentLike;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Message {
+	private final String id;
+	private final boolean prefixed;
+	private final MessageType type;
+	private final Map<String, String> stringReplacements;
+	private final Map<String, ComponentLike> componentReplacements;
+
+	public Message(@NotNull String id, MessageType type, boolean prefixed,
+				   Map<String, String> stringReplacements, Map<String, ComponentLike> componentReplacements) {
+		this.id = id;
+		this.type = type;
+		this.prefixed = prefixed;
+		this.stringReplacements = Collections.unmodifiableMap(stringReplacements);
+		this.componentReplacements = Collections.unmodifiableMap(componentReplacements);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public boolean isPrefixed() {
+		return prefixed;
+	}
+
+	public MessageType getType() {
+		return type;
+	}
+
+	public Map<String, String> getStringReplacements() {
+		return stringReplacements;
+	}
+
+	public Map<String, ComponentLike> getComponentReplacements() {
+		return componentReplacements;
+	}
+
+	public static Message.Builder builder(String id) {
+		return new Message.Builder(id);
+	}
+
+	public enum MessageType {
+		INFO,
+		ERROR,
+		WARNING
+	}
+
+	public static class Builder {
+		private final String id;
+		private boolean prefixed = false;
+		private MessageType type = MessageType.INFO;
+		private final Map<String, String> stringReplacements = new HashMap<>();
+		private final Map<String, ComponentLike> componentReplacements = new HashMap<>();
+
+		public Builder(String id) {
+			this.id = id;
+		}
+
+		public Builder prefixed() {
+			this.prefixed = true;
+			return this;
+		}
+
+		public Builder prefixed(boolean prefixed) {
+			this.prefixed = prefixed;
+			return this;
+		}
+
+		public Builder type(MessageType type) {
+			this.type = type;
+			return this;
+		}
+
+		public Builder replacement(String find, String replace) {
+			stringReplacements.put(find, replace);
+			return this;
+		}
+
+		public Builder replacement(String find, ComponentLike replace) {
+			componentReplacements.put(find, replace);
+			return this;
+		}
+
+		public Message build() {
+			return new Message(id, type, prefixed, stringReplacements, componentReplacements);
+		}
+	}
+}
---
+++ MessagesHelper-velocity/src/main/java/uk/co/notnull/messageshelper/MessagesHelper.java
@@ -0,0 +1,28 @@
+package uk.co.notnull.messageshelper;
+
+import ninja.leaping.configurate.ConfigurationNode;
+
+public class MessagesHelper extends AbstractMessageHelper {
+    private static ConfigurationNode messages;
+    public static void setMessages(ConfigurationNode messages) {
+        MessagesHelper.messages = messages;
+    }
+
+    public static String get(String id) {
+        if(messages == null) {
+            return "";
+        }
+
+        return messages.getNode(id).getString("Message " + id + " does not exist");
+    }
+
+    public static String getPrefix(Message.MessageType messageType) {
+        if(messageType.equals(Message.MessageType.ERROR)) {
+            return MessagesHelper.get("prefix.error");
+        } else if(messageType.equals(Message.MessageType.WARNING)) {
+            return MessagesHelper.get("prefix.warning");
+        } else {
+            return MessagesHelper.get("prefix.info");
+        }
+    }
+}
