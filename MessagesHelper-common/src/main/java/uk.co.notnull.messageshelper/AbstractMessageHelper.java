3ca8344176ac8de732757b777744f67939c169ea
James Lyne <jim+github@not-null.co.uk>
2023-06-16 17:08:50 +0100

    Use instance methods

--- MessagesHelper-common/src/main/java/uk.co.notnull.messageshelper/AbstractMessageHelper.java
+++ MessagesHelper-common/src/main/java/uk.co.notnull.messageshelper/AbstractMessageHelper.java
@@ -9,46 +9,45 @@ import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 
 import java.util.Map;
 
-public abstract class AbstractMessageHelper {
+public abstract class AbstractMessageHelper<C> {
+	protected C messages;
 	public static final MiniMessage miniMessage = MiniMessage.miniMessage();
 
-	public static String getString(String id) {
-        return "Message " + id + " does not exist";
-    }
+    public abstract void setMessages(C messages);
 
-	 public static String getString(Message message) {
+	public abstract String getString(String id);
+
+	public String getString(Message message) {
 		String result = message.isPrefixed() ?
-                getPrefix(message.getType()) + getString(message.getId()) : getString(message.getId());
+				getPrefix(message.getType()) + getString(message.getId()) : getString(message.getId());
 
-		for(Map.Entry<String, String> replacement: message.getStringReplacements().entrySet()) {
+		for (Map.Entry<String, String> replacement : message.getStringReplacements().entrySet()) {
 			result = result.replaceAll(replacement.getKey(), replacement.getValue());
 		}
 
 		return result;
 	}
 
-	public static Component getComponent(Message message) {
+	public Component getComponent(Message message) {
 		String result = message.isPrefixed() ?
-                getPrefix(message.getType()) + getString(message.getId()) : getString(message.getId());
+				getPrefix(message.getType()) + getString(message.getId()) : getString(message.getId());
 
 		TagResolver.Builder placeholders = TagResolver.builder();
 
-		for(Map.Entry<String, String> replacement: message.getStringReplacements().entrySet()) {
+		for (Map.Entry<String, String> replacement : message.getStringReplacements().entrySet()) {
 			placeholders.resolver(Placeholder.parsed(replacement.getKey(), replacement.getValue()));
 		}
 
-		for(Map.Entry<String, ComponentLike> replacement: message.getComponentReplacements().entrySet()) {
+		for (Map.Entry<String, ComponentLike> replacement : message.getComponentReplacements().entrySet()) {
 			placeholders.resolver(Placeholder.component(replacement.getKey(), replacement.getValue()));
 		}
 
 		return miniMessage.deserialize(result, placeholders.build());
 	}
 
-    public static void send(Audience audience, Message message) {
-        audience.sendMessage(getComponent(message));
-    }
-
-	public static String getPrefix(Message.MessageType type) {
-		return "";
+	public void send(Audience audience, Message message) {
+		audience.sendMessage(getComponent(message));
 	}
+
+	public abstract String getPrefix(Message.MessageType type);
 }
